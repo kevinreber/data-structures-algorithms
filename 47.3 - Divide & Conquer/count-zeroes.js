5b61e3ea25987b3c00fe81a3a8a7f14fc6221b0c
kevinreber <kevinreber1@gmail.com>
2020-08-30 00:02:00 -0700

    count zeroes

--- 47.3 - Divide & Conquer/count-zeroes.js
+++ 47.3 - Divide & Conquer/count-zeroes.js
@@ -1,5 +1,23 @@
-function countZeroes() {
-  
+function countZeroes(arr) {
+	let firstZero = findFirst(arr);
+	if (firstZero === -1) return 0;
+
+	return arr.length - firstZero;
 }
 
-module.exports = countZeroes
+function findFirst(arr) {
+	let start = 0;
+	let end = arr.length - 1;
+
+	while (end >= start) {
+		let mid = Math.floor((start + end) / 2);
+		if ((mid === 0 || arr[mid - 1] === 1) && arr[mid] === 0) {
+			return mid;
+		} else if (arr[mid] === 1) {
+			start = mid + 1;
+		} else end = mid - 1;
+	}
+	return -1;
+}
+
+module.exports = countZeroes;
