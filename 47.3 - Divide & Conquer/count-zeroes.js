0ade47f78e78fcd782d55a5c91e9a0c299d27b88
kevinreber <kevinreber1@gmail.com>
2020-08-30 11:24:32 -0700

    count-zeroes

--- 47.3 - Divide & Conquer/count-zeroes.js
+++ 47.3 - Divide & Conquer/count-zeroes.js
@@ -1,3 +1,8 @@
+/**
+ * Given an array of 1s and 0s which has all 1s first followed by all 0s,
+ * write a function called countZeroes, which returns the number of zeroes in the array
+ */
+
 function countZeroes(arr) {
 	let firstZero = findFirst(arr);
 	if (firstZero === -1) return 0;
@@ -19,5 +24,11 @@ function findFirst(arr) {
 	}
 	return -1;
 }
+
+/** Examples */
+// countZeroes([1,1,1,1,0,0]) // 2
+// countZeroes([1,0,0,0,0]) // 4
+// countZeroes([0,0,0]) // 3
+// countZeroes([1,1,1,1]) // 0
 
 module.exports = countZeroes;
