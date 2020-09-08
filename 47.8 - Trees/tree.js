a8f6f46b0762508752d09acaf36e3c9eafbdf031
kevinreber <kevinreber1@gmail.com>
2020-09-07 22:11:42 -0700

    trees

--- 47.8 - Trees/tree.js
+++ 47.8 - Trees/tree.js
@@ -1,35 +1,67 @@
 /** TreeNode: node for a general tree. */
 
 class TreeNode {
-  constructor(val, children = []) {
-    this.val = val;
-    this.children = children;
-  }
+	constructor(val, children = []) {
+		this.val = val;
+		this.children = children;
+	}
 }
 
 class Tree {
-  constructor(root = null) {
-    this.root = root;
-  }
+	constructor(root = null) {
+		this.root = root;
+	}
 
-  /** sumValues(): add up all of the values in the tree. */
+	/** sumValues(): add up all of the values in the tree. */
 
-  sumValues() {
-    
-  }
+	sumValues() {
+		if (!this.root) return null;
 
-  /** countEvens(): count all of the nodes in the tree with even values. */
+		// get value of root node
+		let total = this.root.val;
 
-  countEvens() {
+		function sumHelper(node) {
+			// go through all children for a Node
+			for (let child of node.children) {
+				// accumulate all values
+				total += child.val;
+				// if child has children
+				if (child.children.length > 0) {
+					// recurse with the child as the root
+					return sumHelper(child);
+				}
+			}
+		}
+		sumHelper(this.root);
+		return total;
+	}
 
-  }
+	/** countEvens(): count all of the nodes in the tree with even values. */
 
-  /** numGreater(lowerBound): return a count of the number of nodes
-   * whose value is greater than lowerBound. */
+	countEvens() {
+		if (!this.root) return null;
 
-  numGreater(lowerBound) {
+		let count = this.root.val % 2 === 0 ? 1 : 0;
 
-  }
+		function countEvensHelper(node) {
+			// go through all children for a Node
+			for (let child of node) {
+				// accumulate how many values are even numbers
+				if (child.val % 2 === 0) count++;
+				// if child has any children
+				if ((child, children.length > 0)) {
+					return countEvensHelper(child);
+				}
+			}
+		}
+		countEvensHelper(this.root);
+		return count;
+	}
+
+	/** numGreater(lowerBound): return a count of the number of nodes
+	 * whose value is greater than lowerBound. */
+
+	numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
